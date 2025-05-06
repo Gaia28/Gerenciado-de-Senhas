b2f8031534e8e5c78e442c09e8b605088786f8c6
Gaia28 <matheusgaia33@gmail.com>
2025-05-06 11:11:15 -0300

    #

--- Views/paginaInicial.php
+++ Views/paginaInicial.php
@@ -20,7 +20,7 @@ if(isset($_GET['logout'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/stylePrincipal.css">
     <link rel="stylesheet" href="../styles/styleModal.css">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> -->
+    
     <title>Página Inicial</title>
 </head>
 
